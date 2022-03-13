122eae0a9c78d7aa75d59b1786f6850949148678
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-03-13 18:07:04 -0500

    Climate Action 101 Capstone

---
+++ week7/routes/commentRouter.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const commentRouter = express.Router()
+const Comment = require('../models/comment.js')
+
+
+//Get All
+commentRouter.get('/', (req, res, next) => {
+  Comment.find( (err, comment) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(comment)
+  })
+})
+
+//Get All By Issue Id
+commentRouter.get('/:issueId', (req, res, next) => {
+    Comment.find({issueId: req.params.issueId}, (err, comment) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(comment)
+    })
+  })
+  
+  //Get All By User
+  commentRouter.get('/user', (req, res, next) => {
+      Comment.findOne({user: req.user._id}, (err, comment)=> {
+        if(err){
+          res.status(500)
+          return next(err)
+        }
+        return res.status(200).send(comment)
+      })
+  })
+  
+
+//Post
+commentRouter.post('/', (req, res, next)=> {
+  req.body.user = req.user._id
+  const newComment = new Comment(req.body)
+  newComment.save((err, savedComment) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(201).send(savedComment)
+  })
+})
+
+//Update
+commentRouter.put('/:commentId', (req, res, next)=>{
+  Comment.findOneAndUpdate(
+    {_id: req.params.commentId},
+    req.body,
+    {new: true},
+    (err, updatedComment)=> {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedComment)
+  })
+})
+
+// Like A Comment
+commentRouter.put('/like/:commentId', (req, res, next)=>{
+    Comment.findOneAndUpdate(
+      {_id: req.params.commentId},
+      { $inc: { likes: 1 }},  
+      {new: true},
+      (err, updatedComment)=> {
+        if(err){
+          res.status(500)
+          return next(err)
+        }
+        return res.status(201).send(updatedComment)
+    })
+  })
+
+
+
+//Delete
+commentRouter.delete('/:commentId', (req, res, next)=> {
+  Comment.findOneAndDelete(
+    {_id: req.params.commentId},
+    (err, deletedItem)=>{
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(`Successfully deleted ${deletedItem.comment} from the database.`)
+    }
+  )
+})
+
+module.exports = commentRouter
---
+++ week7/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const expressJwt = require('express-jwt')
+
+
+// Middleware
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+// Connect to DB
+
+mongoose.connect('mongodb://localhost:27017/inventorydb',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  },
+  () => console.log("Connected to the DB")
+)
+
+// Routes
+app.use("/auth", require("./routes/authRouter.js"))
+app.use('/api', expressJwt({secret: process.env.SECRET, algorithms: ['HS256']}))
+app.use("/api/issue", require("./routes/issueRouter.js"))
+app.use("/api/comment", require("./routes/commentRouter.js"))
+
+
+// ERROR HANDLER
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "Unauthorized Error"){
+        res.status(err.status)
+    }
+    return res.send({errMsg: err.message})
+})
+
+
+app.listen(9000, () => {
+    console.log("This server is running on Port 9000")
+})
